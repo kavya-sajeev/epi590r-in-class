679c4ddef44083fc1e4f6fce9561e3ad6aa9f044
kavya-sajeev <kavsajeev@gmail.com>
2025-08-19 15:40:29 -0400

    working sd function

--- functions.R
+++ functions.R
@@ -21,17 +21,70 @@ square<- function(x){
 }
 square(x)
 
+add_two_numbers<- function(x,y){
+	sumval<-x+y
+	return(sumval)
+}
+add_two_numbers(4,6)
+
+a<-4
+b<-6
+add_two_numbers(a,b)
+
+raise<-function(x,power=2){
+	raisedval<-x^power
+	return(raisedval)
+}
+raise(2,3)
+
 p<- c(0,1,1)
 prop<- function(x, multiplier=1) {
 	n<- length(x)
 	mean_val<- multiplier*sum(x)/n
 	return(mean_val)
 }
-prop(p)
-prop(p,100)
 
-add_two_numbers<- function(x,y){
-	sumval<-x+y
-	return(sumval)
+s<- c(1,2,3,4,5)
+
+sd(s)
+
+numval<- sum((s-mean(s))^2)
+sdval<- numval/(length(s)-1)
+sdvalfinal<- sqrt(sdval)
+sdvalfinal
+
+f<- c(1,2,3,4,5,NA)
+x <- f
+standard_dev<- function(x, na.rm=TRUE) {
+	if (na.rm) {
+		x <- na.omit(x)
+		if (length(x)>1) {
+		numval<- sum((x-mean(x))^2)
+		sdval<- numval/(length(x)-1)
+		sdvalfinal<- sqrt(sdval)
+		return(sdvalfinal)
+		}
+		else {
+			sdvalfinal<-NA
+			return(sdvalfinal)
+		}}
+	else {
+		sdvalfinal<-NA
+		return(sdvalfinal)
+	}
 }
-add_two_numbers(4,6)
+standard_dev(f,na.rm=FALSE)
+standard_dev(nlsy$income, na.rm=FALSE)
+
+
+
+
+
+
+
+
+
+
+
+
+
